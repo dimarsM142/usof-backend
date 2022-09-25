0c6112c88d5bc0a03d1357ca9cef058c8dcad114
dimarsM142 <dimonars11032003@gmail.com>
2022-09-26 00:35:11 +0300

    heroku

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const configPath = './config.json';
 const parsed = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: parsed.host,
     user: parsed.user,
     database: parsed.database,
