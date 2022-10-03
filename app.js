fd4414ade1cc4d1f110d271d51108b160c8027cf
dimarsM142 <dimonars11032003@gmail.com>
2022-10-03 23:39:49 +0300

    Connection

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const apiUsersRoutes = require('./Routes/api-users.js');
 const apiPostsRoutes = require('./Routes/api-posts.js');
 const apiCategoriesRoutes = require('./Routes/api-categories');
 const apiCommentsRoutes = require('./Routes/api-comments');
+const cors = require('cors');
 
+const corsOptions ={
+    origin:['http://localhost:3000', 'http://localhost:3001'], 
+    credentials:true,
+    methods:["GET" , "POST" , "PUT", "DELETE"],
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
